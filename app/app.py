81ad50a559f97ec2e5f77a6d45c4ec2de651c914
DipenChawla <dipenchawla86@gmail.com>
2020-06-01 04:08:32 +0530

    changed_app.py(pipeline testing)

--- app/app.py
+++ app/app.py
@@ -1,31 +1,37 @@
-"""  flask app for sentiment prediction"""
-import pickle
 import numpy as np
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
+import pickle
 
 app = Flask(__name__)
-model = pickle.load(open("model.pkl", "rb"))
+model = pickle.load(open('model.pkl', 'rb'))
 
-
-@app.route("/")
+@app.route('/')
 def home():
-    """
-    default response
-    """
-    return "Welcome to the Udacity Cloud Nanodegree Capstone"
+    return render_template('index.html')
 
+@app.route('/predict',methods=['POST'])
+def predict():
+    '''
+    For rendering results on HTML GUI
+    '''
+    int_features = [int(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
 
-@app.route("/predict_api", methods=["POST"])
+    output = round(prediction[0], 2)
+
+    return render_template('index.html', prediction_text='Employee Salary should be $ {}'.format(output))
+
+@app.route('/predict_api',methods=['POST'])
 def predict_api():
-    """
+    '''
     For direct API calls trought request
-    """
+    '''
     data = request.get_json(force=True)
     prediction = model.predict([np.array(list(data.values()))])
 
     output = prediction[0]
     return jsonify(output)
 
-
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(debug=True)
---
+++ scripts/old_app.py
@@ -0,0 +1,31 @@
+"""  flask app for sentiment prediction"""
+import pickle
+import numpy as np
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+model = pickle.load(open("model.pkl", "rb"))
+
+
+@app.route("/")
+def home():
+    """
+    default response
+    """
+    return "Welcome to the Udacity Cloud Nanodegree Capstone"
+
+
+@app.route("/predict_api", methods=["POST"])
+def predict_api():
+    """
+    For direct API calls trought request
+    """
+    data = request.get_json(force=True)
+    prediction = model.predict([np.array(list(data.values()))])
+
+    output = prediction[0]
+    return jsonify(output)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
